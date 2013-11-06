f20621e9ccc36f6ee1475fb60c9ce3b8954f774a
tkshredder <alex@panyamania.com>
2013-11-06 16:36:24 -0500

    Building out UI - move between characters

---
+++ Assets/Scripts/CommandPanel.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class CommandPanel : MonoBehaviour {
+	
+	public List<GameObject> _commandButtons;
+	public UILabel _descriptionLabel;
+	public UILabel _outcomeLabel;
+	
+	
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+	
+	public void setDescription (string desc) {
+		
+		_descriptionLabel.text = desc; 
+	}
+}
---
+++ Assets/Scripts/SelectCrewMember.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+//using Holoville.HOTween;
+//using Holoville.HOTween.Plugins;
+	
+public class SelectCrewMember : MonoBehaviour {
+	
+	private int _currentCrewIndex;
+	
+	public GameObject _shipCamera;
+	public List<GameObject> crewMembers;
+	public CommandPanel _commandPanel;
+	
+	
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetButtonUp("Horizontal") && Input.GetAxis("Horizontal") < 0) {
+			
+			_currentCrewIndex--;
+			if (_currentCrewIndex < 0) {
+				_currentCrewIndex = crewMembers.Count-1;
+			}
+		}
+		if (Input.GetButtonUp("Horizontal") && Input.GetAxis("Horizontal") > 0) {
+			_currentCrewIndex++;
+			if (_currentCrewIndex >= crewMembers.Count) {
+				_currentCrewIndex = 0;
+			}
+		}
+		
+		// Move the camera to the x position of the current cube:
+		//Camera.main.transform.position.x = crewMembers[_currentCrewIndex].transform.position.x;
+		//HOTween.To(_shipCamera.transform, 0.2f, "position", new Vector3(crewMembers[_currentCrewIndex].transform.position.x,_shipCamera.transform.position.y,_shipCamera.transform.position.z));
+		
+		iTween.MoveTo(_shipCamera, new Vector3(crewMembers[_currentCrewIndex].transform.position.x,_shipCamera.transform.position.y,_shipCamera.transform.position.z), 0.2f);
+		
+		_commandPanel.setDescription(_currentCrewIndex.ToString());
+		
+	}
+}
